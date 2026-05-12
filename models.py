1555456a1ec7ff1550c5be1ecef669db8e40663d
Tristaros <malo.rinckel@gmail.com>
2026-05-12 21:06:58 +0000

    Nettoyage modèle Exercise

--- models.py
+++ models.py
@@ -9,26 +9,12 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    workouts = db.relationship("Workout", backref="user", lazy=True)
 
     def set_password(self, password: str):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password: str) -> bool:
         return check_password_hash(self.password_hash, password)
-
-
-class Workout(db.Model):
-    __tablename__ = "workouts"
-
-    id = db.Column(db.Integer, primary_key=True)
-
-    name = db.Column(db.String(100), nullable=False)
-
-    date = db.Column(db.DateTime, server_default=db.func.now())
-
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-
 
 class Exercise(db.Model):
     __tablename__ = "exercises"
@@ -38,5 +24,3 @@ class Exercise(db.Model):
     name = db.Column(db.String(100), nullable=False)
 
     muscle = db.Column(db.String(50), nullable=False)
-
-    workout_id = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=False)
