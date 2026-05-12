fbf39c9d4ab552b0ebb7c4e6d1c3409398d6f403
reibelbenjamin6-dev <reibelbenjamin6@gmail.com>
2026-05-12 20:52:23 +0000

    Ajout templates et navigation

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
